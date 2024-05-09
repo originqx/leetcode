7bc0b1e05835db1a2fec3c9f0fc62f15f7c3e0d9
祁旋 <1059716287@qq.com>
2024-05-09 16:38:08 +0800

    141.环形链表

---
+++ src/main/java/org/example/linkedlist/HasCycle.java
@@ -0,0 +1,32 @@
+package org.example.linkedlist;
+
+public class HasCycle {
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        node.next.next.next.next.next = node.next.next;
+        Solution6 solution6 = new Solution6();
+        System.out.println(solution6.hasCycle(node));
+    }
+}
+
+class Solution6 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode quick = head.next;
+        while (quick != slow) {
+            if (quick == null || quick.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            quick = quick.next.next;
+        }
+        return true;
+    }
+}
