ddbf723586ddecc253f35c268ebf4ff75503219c
祁旋 <1059716287@qq.com>
2024-05-17 16:50:00 +0800

    94. 二叉树的中序遍历

---
+++ src/main/java/org/example/binarytree/InorderTraversal.java
@@ -0,0 +1,65 @@
+package org.example.binarytree;
+
+import java.util.*;
+
+public class InorderTraversal {
+    public static void main(String[] args) {
+        List<Integer> integers = new Solution2().inorderTraversal(
+                new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3))
+        );
+        integers.forEach(System.out::println);
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// 递归
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
+
+// 迭代
+class Solution2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
+
+
