da8e2cf05dd5944ed150eae294d516cb2b43bfb6
祁旋 <1059716287@qq.com>
2024-05-21 14:15:37 +0800

    208. 实现 Trie (前缀树)

---
+++ src/main/java/org/example/graph/Triew.java
@@ -0,0 +1,59 @@
+package org.example.graph;
+
+public class Triew {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple")); // 返回 true
+        System.out.println(trie.search("app")); // 返回 false
+        trie.insert("app");
+        System.out.println(trie.search("app")); // 返回 true
+        System.out.println(trie.startsWith("app")); // 返回 true
+    }
+}
+
+class Trie {
+    private Trie[] children;
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                Trie trie = new Trie();
+                node.children[index] = trie;
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie trie = searchPrefix(word);
+        return trie != null && trie.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
