d462b79fa01baf6ea3259dc3465538e2799f7b96
祁旋 <1059716287@qq.com>
2024-05-21 14:16:24 +0800

    114. 二叉树展开为链表(Weiwancheng )

---
+++ src/main/java/org/example/binarytree/Flatten.java
@@ -0,0 +1,13 @@
+package org.example.binarytree;
+
+public class Flatten {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution16 {
+    public void flatten(TreeNode root) {
+
+    }
+}
