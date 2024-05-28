20f5da045b25b9a807b092f6163a69d538e7b7a0
祁旋 <1059716287@qq.com>
2024-05-28 22:51:40 +0800

    198. 打家劫舍

---
+++ src/main/java/org/example/dynamic/Rob.java
@@ -0,0 +1,27 @@
+package org.example.dynamic;
+
+public class Rob {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 3, 1};
+        Solution7 solution7 = new Solution7();
+        System.out.println(solution7.rob(nums));
+    }
+}
+
+class Solution7 {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
