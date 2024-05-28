531eb58af1effa9fd8b5aecb7f386fce6a9bd293
祁旋 <1059716287@qq.com>
2024-05-28 18:45:52 +0800

    70. 爬楼梯

---
+++ src/main/java/org/example/dynamic/ClimbStairs.java
@@ -0,0 +1,23 @@
+package org.example.dynamic;
+
+public class ClimbStairs
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(5)); // 输出 8
+    }
+}
+
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
