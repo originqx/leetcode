6508f450201cdb432f43073a644323340ba3f211
祁旋 <1059716287@qq.com>
2024-05-29 14:53:23 +0800

    139. 单词拆分

--- src/main/java/org/example/dynamic/WordBreak.java
+++ src/main/java/org/example/dynamic/WordBreak.java
@@ -1,14 +1,20 @@
 package org.example.dynamic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordBreak {
     public static void main(String[] args) {
-
+        String s = "leetcode";
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        Solution4 solution = new Solution4();
+        System.out.println(solution.wordBreak(s, wordDict));
     }
 }
 
-class Solution {
+class Solution4 {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
