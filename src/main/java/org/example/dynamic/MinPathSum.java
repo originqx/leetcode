aff7a3e2377baf4749bd17568b42e255498341a7
祁旋 <1059716287@qq.com>
2024-05-29 15:39:32 +0800

    64. 最小路径和

---
+++ src/main/java/org/example/dynamic/MinPathSum.java
@@ -0,0 +1,34 @@
+package org.example.dynamic;
+
+import javax.swing.*;
+
+public class MinPathSum {
+    public static void main(String[] args) {
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        Solution12 solution = new Solution12();
+        System.out.println(solution.minPathSum(grid));
+    }
+}
+
+class Solution12 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+//                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
